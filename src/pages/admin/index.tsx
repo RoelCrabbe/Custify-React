7d769c7ac62ed8e9114e2de4624b62af504f0466
Roel Crabbé <crabberoel@gmail.com>
2025-06-08 12:11:04 +0200

    missing key

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -53,7 +53,7 @@ const AdminLandingPage: React.FC = () => {
 
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
                             {adminQuickStats.map((quickStat) => (
-                                <FormContainer.StatCard isVisible={isVisible}>
+                                <FormContainer.StatCard isVisible={isVisible} key={quickStat.value}>
                                     <h3 className="text-2xl font-semibold text-gray-900">
                                         {quickStat.value}
                                     </h3>
